b5da2ab2f32ca786dcd3b7b6520b2f9992895ff9
Santiago Sanchez <sgsanchez.90@gmail.com>
2024-03-14 09:46:33 -0300

    Bug caracter vacio o multiples caracteres corregido. Cambio de numero de intentos a numero de fallos. Se indica cantidas de intentos restantes por terminal

--- game.py
+++ game.py
@@ -17,35 +17,45 @@ word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+#Contador de intentos restantes
+remaining_attempts = max_attempts
+while remaining_attempts > 0:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     
     # Verificar si la letra ya ha sido adivinada
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
-        continue
-    # Agregar la letra a la lista de letras adivinadas
-    guessed_letters.append(letter)
-    # Verificar si la letra está en la palabra secreta
-    if letter in secret_word:
-        print("¡Bien hecho! La letra está en la palabra.")
-    else:
-        print("Lo siento, la letra no está en la palabra.")
-    # Mostrar la palabra parcialmente adivinada
-    letters = []
-    for letter in secret_word:
-        if letter in guessed_letters:
-            letters.append(letter)
+    # Verificar si ingresó una letra
+    elif not letter:
+        print ("Por favor, ingrese una letra")
+    # Verificar si ingresó más de una letra
+    elif len(letter) > 1:
+        print ("Por favor, ingrese solo una letra")
+    else: 
+        # Agregar la letra a la lista de letras adivinadas
+        guessed_letters.append(letter)
+        # Verificar si la letra está en la palabra secreta
+        if letter in secret_word:
+            print("¡Bien hecho! La letra está en la palabra.")
         else:
-            letters.append("_")
-    
+            print("Lo siento, la letra no está en la palabra.")
+            # Disminuir la cantidad de intentos restantes únicamente en caso de fallos
+            remaining_attempts -=1
+        # Mostrar la palabra parcialmente adivinada
+        letters = []
+        for letter in secret_word:
+            if letter in guessed_letters:
+                letters.append(letter)
+            else:
+                letters.append("_")
     word_displayed = "".join(letters)
     print(f"Palabra: {word_displayed}")
+    # Indicar la cantidad de intentos restantes
+    print (f"Intentos restantes: {remaining_attempts}")
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
-        print(f"¡Felicidades! Has adivinado la palabra 
-              secreta: {secret_word}")
+        print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
     print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
